d0b3797135e1e925c84a25d3f14dce0b76ea6b99
JrSchoolAccount <jesperx2000@gmail.com>
2023-12-29 16:04:06 +0100

    Styling for the herobanner

--- herobanner.js
+++ herobanner.js
@@ -1,53 +1,21 @@
-var current = 0;
-const item = document.querySelectorAll('.herobanner_item');
-const dot = document.querySelectorAll('.dot');
+let slideIndex = 0;
+showSlides();
 
-function reset() {
-  for (i = 0; i < item.length; i++) {
-    item[i].style.display = 'none';
-    dot[i].style.background = '#e6e6e6';
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName('herobanner_item');
+  let dots = document.getElementsByClassName('dot');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
   }
-}
-//previous arrow
-function previousSlide() {
-  reset();
-  if (current == 0) {
-    current = item.length;
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
   }
-  item[current - 1].style.display = 'block';
-  dot[current - 1].style.background = 'transparent';
-  current--;
-}
-//next arrow
-function nextSlide() {
-  reset();
-  if (current == item.length - 1) {
-    current = -1;
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
   }
-  item[current + 1].style.display = 'block';
-  dot[current + 1].style.background = 'transparent';
-  current++;
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
-
-function currentSlide(i) {
-  reset();
-  item[i].style.display = 'block';
-  dot[i].style.background = 'transparent';
-  current = i;
-}
-
-dot1 = document
-  .getElementById('dot1')
-  .addEventListener('click', currentSlide(0));
-dot2 = document
-  .getElementById('dot2')
-  .addEventListener('click', currentSlide(1));
-dot3 = document
-  .getElementById('dot3')
-  .addEventListener('click', currentSlide(2));
-dot4 = document
-  .getElementById('dot4')
-  .addEventListener('click', currentSlide(3));
-dot5 = document
-  .getElementById('dot5')
-  .addEventListener('click', currentSlide(4));
