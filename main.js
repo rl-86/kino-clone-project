91229542b27fc7484deddc5afc9c1631512b5bba
rl-86 <rl86@live.se>
2024-01-27 14:35:06 +0100

    Installed handlebars-layouts to implement header and footer

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
 import { showSlides } from "./herobanner.js";
-// Function to add same header and footer to all html pages with id="header" or "footer"
-async function includeHTML() {
-  // Fetch the header HTML
-  const headerFetch = await fetch("templates/header.html");
-  const headerHtml = await headerFetch.text();
-  document.querySelector("header").innerHTML = headerHtml;
-
-  // Fetch the footer HTML
-  const footerFetch = await fetch("templates/footer.html");
-  const footerHtml = await footerFetch.text();
-  document.querySelector("footer").innerHTML = footerHtml;
-}
-
-includeHTML();
 
 // Herobanner slideshow
 showSlides();
--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { engine } from "express-handlebars";
+import exphbs from "express-handlebars";
+import handlebarsLayouts from "handlebars-layouts";
 import { loadMovie, loadMovies } from "./movies.js";
 
 const app = express();
 
-app.engine("handlebars", engine());
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: ".handlebars",
+  helpers: handlebarsLayouts(),
+});
+
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.set("views", "./templates");
 
