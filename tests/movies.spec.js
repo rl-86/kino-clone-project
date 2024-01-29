fdfdffdeb5e05a8bba58b9372f50991e6b911e5d
rl-86 <rl86@live.se>
2024-01-29 09:13:09 +0100

    Integration test for all movie titles

--- tests/movies.spec.js
+++ tests/movies.spec.js
@@ -1,12 +1,23 @@
 import { test } from "@jest/globals";
 import request from "supertest";
 import app from "../src/app.js";
+import { loadMovies } from "../src/movies.js";
 
-test("Encanto page shows title of movie", async () => {
-  const response = await request(app)
-    .get("/movie/2")
-    .expect("Content-Type", "text/html; charset=utf-8")
-    .expect(200);
+test("All movie titles are displayed on the movies page", async () => {
+  const movies = await loadMovies();
+  const passedTitles = [];
+  let titleCount = 0;
 
-  expect(response.text).toMatch("Encanto");
+  for (const movie of movies) {
+    const response = await request(app)
+      .get(`/movie/${movie.id}`)
+      .expect("Content-Type", "text/html; charset=utf-8")
+      .expect(200);
+
+    expect(response.text).toMatch(movie.title);
+
+    passedTitles.push(movie.title);
+    titleCount++;
+  }
+  console.log("Titles passed:", titleCount, passedTitles);
 });
